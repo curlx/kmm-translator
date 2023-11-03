41b3314debbf0783dd181b471be38c3535ab7ada
pijak.jirapiwong <pijak.jirapiwong@mobimeo.com>
2023-11-03 13:39:35 +0100

    add translation views to iOS app

---
+++ iosApp/iosApp/translate/presentation/components/LanguageDisplay.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+import shared
+
+struct LanguageDisplay: View {
+    var language: UiLanguage
+    
+    var body: some View {
+        HStack {
+            SmallLanguageIcon(language: language)
+                .padding(.trailing, 5)
+            Text(language.language.langName)
+                .foregroundColor(.lightBlue)
+        }
+    }
+}
+
+struct LanguageDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguageDisplay(
+            language: UiLanguage(language: .german, imageName: "german")
+        )
+    }
+}
